5e69a65ebd37a00a2a59b0326e6337f6acfce57b
João Moreira <joaomoreirati98@gmail.com>
2025-07-20 15:51:54 -0300

    :construction: Cadastrando usuário no banco de dados

--- cadastro.php
+++ cadastro.php
@@ -3,4 +3,8 @@ include_once("conexao.php");
 
 $nome = filter_input(INPUT_POST,"nome",FILTER_SANITIZE_STRING);
 $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
+
+$inserirUsuario = "INSERT INTO usuario (nome, email) VALUES ('$nome', '$email')";
+
+$cadastro = mysqli_query($conexao, $inserirUsuario);
 ?>
