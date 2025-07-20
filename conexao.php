2b90cdd20abd0482848eb331fe182583e9e35ce1
João Moreira <joaomoreirati98@gmail.com>
2025-07-20 15:36:29 -0300

    :sparkles: Criando a conexão com o banco de dados

---
+++ conexao.php
@@ -0,0 +1,8 @@
+<?php
+$servidor = "localhost";
+$usuarioDoBancoDeDados = "root";
+$senha = "";
+$nomeDoBancoDeDados = "projeto";
+
+$conexao = mysqli_connect($servidor, $usuarioDoBancoDeDados, $senha, $nomeDoBancoDeDados);
+?>
