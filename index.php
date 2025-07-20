2bc9062253691c38d3c91a59162f4754623573b7
João Moreira <joaomoreirati98@gmail.com>
2025-07-20 16:17:06 -0300

    :construction: Adicionando campos de endereço ao formulário

--- index.php
+++ index.php
@@ -8,12 +8,27 @@
     <body>
         <h1>CADASTRO DE USUÁRIO</h1>
 
+        <h2>Dados Pessoais</h2>
         <form action="cadastro.php" method= "POST">
             <label>Nome:</label>
             <input type = "text" id="nome" name ="nome">
 
             <label>E-mail:</label>
             <input type = "email" id="email" name = "email">
+            <hr>
+
+        <h2>Endereço</h2>
+
+        <label>CEP:</label>
+        <input type = "text" id="cep" name ="cep">
+        <label>Rua:</label>
+        <input type = "text" id="rua" name ="rua">
+        <label>Bairro:</label>
+        <input type = "text" id="bairro" name ="bairro">
+        <label>Cidade:</label>
+        <input type = "text" id="cidade" name ="cidade">
+        <label>Estado:</label>
+        <input type = "text" id="estado" name ="estado"> <br><br>
 
             <input type= "submit" value = "Cadastrar">
 
