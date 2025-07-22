48674b9a458cd58f0bd24c9812c6da35d0ada28a
João Moreira <joaomoreirati98@gmail.com>
2025-07-21 21:27:39 -0300

    :construction: Adicionando listagem de cadastro

---
+++ listar.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Listagem de cadastro</title>
+        <meta charset = "utf-8">
+    </head>
+
+    <body>
+        <h1>Listagem de cadastro</h1>
+
+        <?php
+            $listagem = "SELECT * from usuario";
+            
+        ?>
+    </body>
+</html>
