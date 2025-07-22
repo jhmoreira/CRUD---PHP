35109b0590070e217731240d180eb33e511b8545
João Moreira <joaomoreirati98@gmail.com>
2025-07-21 21:39:03 -0300

    :construction: Corrigindo erro ao inserir dados do formulário de endereço no banco de dados

--- cadastro.php
+++ cadastro.php
@@ -9,7 +9,7 @@ $bairro = filter_input(INPUT_POST,"bairro",FILTER_SANITIZE_STRING);
 $cidade = filter_input(INPUT_POST,"cidade",FILTER_SANITIZE_STRING);
 $estado = filter_input(INPUT_POST,"estado",FILTER_SANITIZE_STRING);
 
-$inserirUsuario = "INSERT INTO usuario (nome, email, cep, rua, bairro, cidade, estado) VALUES ('$nome', '$email', '$cep', '$rua', '$bairro', $cidade', '$estado')";
+$inserirUsuario = "INSERT INTO usuario (nome, email, cep, rua, bairro, cidade, estado) VALUES ('$nome', '$email', '$cep', '$rua', '$bairro', '$cidade', '$estado')";
 
 $cadastro = mysqli_query($conexao, $inserirUsuario);
 ?>
