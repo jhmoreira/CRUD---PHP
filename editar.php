eb54f8f0be3a3eb10e9928829bc2e37acf56c45c
João Moreira <joaomoreirati98@gmail.com>
2025-07-24 13:09:59 -0300

    :construction: Adicionando formulário de edição do usuário

---
+++ editar.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> CRUD PHP</title>
+        <meta charset = "UTF-8">
+    </head>
+
+    <body>
+        <h1>EDITAR  USUÁRIO</h1>
+
+        <h2>Dados Pessoais</h2>
+        <form action="editar_usuario.php" method= "POST">
+            <label>Nome:</label>
+            <input type = "text" id="nome" name ="nome">
+
+            <label>E-mail:</label>
+            <input type = "email" id="email" name = "email">
+            <hr>
+
+        <h2>Endereço</h2>
+
+        <label>CEP:</label>
+        <input type = "text" id="cep" name ="cep" onblur="pesquisacep(this.value);">
+        <label>Rua:</label>
+        <input type = "text" id="rua" name ="rua">
+        <label>Bairro:</label>
+        <input type = "text" id="bairro" name ="bairro">
+        <label>Cidade:</label>
+        <input type = "text" id="cidade" name ="cidade">
+        <label>Estado:</label>
+        <input type = "text" id="estado" name ="estado"> <br><br>
+
+            <input type= "submit" value = "Atualizar">
+
+        </form>
+
+        <script src = "js/api.js"></script>
+    </body>
+</html>
