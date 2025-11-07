2b69cb7e2641da2521e61a3763a0394e49325b92
João Moreira <joaomoreirati98@gmail.com>
2025-11-07 11:21:27 -0300

    :lipstick: Aplicando modifições de estilo na listagem de cadastros

--- listar.php
+++ listar.php
@@ -10,29 +10,59 @@ include_once("conexao.php");
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
     </head>
 
-    <body>
-        <h1>Listagem de cadastro</h1>
+    <body class="bg-light">
+        <div class="container py-5">
+            <div class="d-flex justify-content-between align-items-center mb-4"></div>
+        <h1 class="text-primary fw-bold">Usuários cadastrados</h1>
+        <a href="index.php" class="btn btn-success">Adicionar Usuário</a>
+        </div>
 
-        <?php
-            echo "<a href = 'index.php'>Cadastrar</a><br><br>";
+        <div class = "card shadow-sm">
+            <div class="card-body">
+                <table class="table table-hover align-middle mb-0">
+                    <thead class="table-primary">
+                        <tr>
+                            <th>Nome</th>
+                            <th>E-mail</th>
+                            <th>CEP</th>
+                            <th>Rua</th>
+                            <th>Bairro</th>
+                            <th>Cidade</th>
+                            <th>Estado</th>
+                            <th class="text-center">Ações</th>
+
+                        </tr>
+                    </thead>
+                
+            </div>
+        </div>
+
+        <tbody>
+            <?php
             $listagem = "SELECT * from usuario";
             $listaDeCadastro = mysqli_query($conexao, $listagem);
-
-            while($cadastro=mysqli_fetch_assoc($listaDeCadastro)){
-
-                echo "Nome: ". $cadastro["nome"]. "<br>";
-                echo "E-mail: ". $cadastro["email"]. "<br>";
-                echo "CEP: : ". $cadastro["cep"]. "<br>";
-                echo "Rua: ". $cadastro["rua"]. "<br>";
-                echo "Bairro: ". $cadastro["bairro"]. "<br>";
-                echo "Cidade: ". $cadastro["cidade"]. "<br>";
-                echo "Estado: ". $cadastro["estado"]. "<br>";
-                echo "<a href = 'editar.php?id= ".$cadastro['id']."'> Editar</a>";
-                echo "<a href = 'deletar.php?id= ".$cadastro['id']."'> Deletar</a><hr>";
+            while($cadastro=mysqli_fetch_assoc($listaDeCadastro)){?>
+            <tr>
+                <td><?= htmlspecialchars($cadastro['nome'])?></td>
+                <td><?=htmlspecialchars($cadastro['email'])?></td>
+                <td><?=htmlspecialchars($cadastro['cep'])?></td>
+                <td><?=htmlspecialchars($cadastro['rua'])?></td>
+                <td><?=htmlspecialchars($cadastro['bairro'])?></td>
+                <td><?=htmlspecialchars($cadastro['cidade'])?></td>
+                <td><?=htmlspecialchars($cadastro['estado'])?></td>
+                <td class="text-center">
+                    <a href = "editar.php?id= <?=$cadastro['id']?>" class="btn btn-warning btn-sm me-2"> Editar</a>
+                    <a href = "deletar.php?id=<?=$cadastro['id']?>" class="btn btn-danger btn-sm"> Deletar</a>
+                </td>
                 
-
-            }
-            
-        ?>
+                
+            </tr>
+            <?php } ?>
+        </tbody>
+            </table>
+        
+        </div>
+            </div>
+            </div>
     </body>
 </html>
