f3e41bf9a7a3a2fcf6a44e3e0fe19c7bb7888a14
João Moreira <joaomoreirati98@gmail.com>
2025-11-07 10:48:48 -0300

    :lipstick: Aplicando modifições de estilo na página principal

--- index.php
+++ index.php
@@ -7,35 +7,53 @@
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
     </head>
 
-    <body>
-        <h1>CADASTRO DE USUÁRIO</h1>
-        <a href = 'listar.php'>Listar</a><br>
-        <h2>Dados Pessoais</h2>
+    <body class = "bg-light">
+        <div class="container py-5">
+            <div class="card shadow-lg border-0 rounded-4">
+                <div class="card-body p-5">
+                    <h1 class="text-center mb-4 text-primary fw-bold">CADASTRO DE USUÁRIO</h1>
+    
+        <h2 class="text-secondary mt-3">Dados Pessoais</h2>
         <form action="cadastro.php" method= "POST">
-            <label>Nome:</label>
-            <input type = "text" id="nome" name ="nome">
+            <div class="col-md-6">
+            <label class="form-label">Nome:</label>
+            <input type = "text" id="nome" name ="nome" class="form-control">
+            </div>
+            <div class="col-md-6">
+            <label class="form-label">E-mail:</label>
+            <input type = "email" id="email" name = "email" class="form-control">
+            <hr class ="mt-4">
+            </div>
 
-            <label>E-mail:</label>
-            <input type = "email" id="email" name = "email">
-            <hr>
-
-        <h2>Endereço</h2>
-
+        <h2 class="text-secondary mt-3">Endereço</h2>
+        <div class="col-md-4">
         <label>CEP:</label>
-        <input type = "text" id="cep" name ="cep" onblur="pesquisacep(this.value);">
+        <input type = "text" id="cep" name ="cep" class="form-control" onblur="pesquisacep(this.value);">
+        </div>
+        <div class="col-md-8">
         <label>Rua:</label>
-        <input type = "text" id="rua" name ="rua">
+        <input type = "text" id="rua" name ="rua" class="form-control">
+        </div>
+        <div class="col-md-5">
         <label>Bairro:</label>
-        <input type = "text" id="bairro" name ="bairro">
+        <input type = "text" id="bairro" name ="bairro" class="form-control">
+        </div>
+        <div class="col-md-5">
         <label>Cidade:</label>
-        <input type = "text" id="cidade" name ="cidade">
+        <input type = "text" id="cidade" name ="cidade" class="form-control">
+        </div>
+        <div class="col-md-2">
         <label>Estado:</label>
-        <input type = "text" id="estado" name ="estado"> <br><br>
-
-            <input type= "submit" value = "Cadastrar">
-
+        <input type = "text" id="estado" name ="estado" class="form-control">
+        </div>
+        <div class = "text-center mt-4">
+            <button type= "submit" class = "btn btn-primary btn-lg px-5">Cadastrar</button>
+            <a href = 'listar.php' class = "btn btn-outline btn-lg px-5 ms-2"> Ver lista</a>
+        </div>
         </form>
-
+        </div>
+        </div>
+    </div>
         <script src = "js/api.js"></script>
     </body>
 </html>
